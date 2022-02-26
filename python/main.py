f32b3e888b24230be0acee7eb4aa407236b4afd7
AlePPisa <aleppaulon@gmail.com>
2022-02-26 12:54:58 -0800

    Botler can now respond to unknown comments

--- python/main.py
+++ python/main.py
@@ -7,7 +7,18 @@ from language_pairs import pairs
 def Botler():
     print("Hi, I'm Botler want to have a conversation with you!\nPlease speak to me in lowercase English. Type quit if you wish to end this conversation. ")
     chat = Chat(pairs, reflections)
-    chat.converse()
+
+    user_input = input()
+    while user_input != "quit":
+        response = chat.respond(user_input)
+        if response:
+            print(response)
+        else:
+            print("Sorry sir, I didn't understand")
+
+        user_input = input()
+
+    print("Thank you for chatting with me")
 
 
 def main():
