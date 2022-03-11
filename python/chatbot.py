06b5c3845b43c105626b131750997e3b4d7c7bf4
AlePPisa <aleppaulon@gmail.com>
2022-03-11 13:21:41 -0800

    Refactored spell-check

--- python/chatbot.py
+++ python/chatbot.py
@@ -14,6 +14,8 @@ class Botler:
         """Creates chat object from NLTK"""
         self.chat = Chat(pairs, reflections)
         self.name = "Botler"
+        self.sentiment_analyzer = SentimentIntensityAnalyzer()
+        self.speller = Speller()
        
     def converse(self):
         """THIS IS NOW DEPRECATED
@@ -23,37 +25,45 @@ class Botler:
         print("Hi, I'm Botler want to have a conversation with you!\nPlease speak to me in lowercase English. Type "
               "quit if you wish to end this conversation. ")
         
-        #starting SIA from NLTK package.No usage implemented yet.
+        # starting SIA from NLTK package.No usage implemented yet.
         sia = SentimentIntensityAnalyzer()
         
-        #speller() parses user input for errors and corrects.
+        # speller() parses user input for errors and corrects.
         spell = Speller()
         user_input = input()
         cleaned_input = spell(user_input).lower()
 
-
         while cleaned_input != "quit":
-            #if for notfying user their input has been corrected
-            if(str(user_input).lower() != str(cleaned_input)):
+            # if for notifying user their input has been corrected
+            if str(user_input).lower() != str(cleaned_input):
                 print("Spelling mistakes were detected in your text, you said:\n " + str(user_input) + "\nAnd it was corrected to: \n" + str(cleaned_input) )
             
             response = self.chat.respond(cleaned_input)
             
-            #if/else for handling no pre-programmed response
-            if(str(response) != "None"):
+            # if/else for handling no pre-programmed response
+            if str(response) != "None":
                 print(sia.polarity_scores(cleaned_input))
                 print(response)    
             else:
                 print(sia.polarity_scores(cleaned_input))
                 print("Sorry sir, I didn't understand")
 
-            #setting new inputs for loop
+            # setting new inputs for loop
             user_input = input()
             cleaned_input = spell(user_input).lower()
 
         print("Thank you for chatting with me")
 
     def generate_response(self, msg):
-        """Generates a response for a specific message"""
-        response = self.chat.respond(msg)
+        """Generates a response for a specific message after correcting any spelling mistakes"""
+
+        # Correct any spelling mistakes
+        clean_input = self.speller(msg)
+
+        # Generate a response from the chatbot
+        response = self.chat.respond(clean_input)
+
+        # Print polarity score of message
+        print(clean_input, self.sentiment_analyzer.polarity_scores(clean_input), sep=": ")
+
         return response if response else "Sorry sir, I didn't understand"
